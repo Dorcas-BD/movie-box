08e0989fe66a6dcfd952abe7c77a740794335ac1
Dorcas Bamisile <bamisiledorcas@gmail.com>
2023-09-13 23:36:45 +0100

    MovieCard

--- components/MovieCard/MovieCard.js
+++ components/MovieCard/MovieCard.js
@@ -1,9 +1,19 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import "./MovieCard.css";
 
 const MovieCard = ({ movies }) => {
+  const [favoriteStatus, setFavoriteStatus] = useState(
+    Array(movies.length).fill(false)
+  );
+
+  const toggleFavorite = (index) => {
+    const updatedFavoriteStatus = [...favoriteStatus];
+    updatedFavoriteStatus[index] = !updatedFavoriteStatus[index];
+    setFavoriteStatus(updatedFavoriteStatus);
+  };
+
   if (!movies || movies.length === 0) {
     return (
       <div className="movie-card" data-testid="movie-card">
@@ -12,6 +22,7 @@ const MovieCard = ({ movies }) => {
     );
   }
 
+  console.log(movies);
   return (
     <div className="movie-card" data-testid="movie-card">
       {movies.map((movie, index) => (
@@ -22,10 +33,28 @@ const MovieCard = ({ movies }) => {
               alt="movie"
               data-testid="movie-poster"
             />
+            <p data-testid="movie-release-year"> {movie.release_date}</p>
             <h2 className="movie-title" data-testid="movie-title">
               {movie.title}
             </h2>
-            <p data-testid="movie-release-date">{movie.releaseYear}</p>
+            {/* <p data-testid="movie-overview" className="movie-overview">
+              {movie.overview}
+            </p> */}
+            <p data-testid="movie-genre">{movie.genre}</p>
+            <div className="rating">
+              <img src="imdb.svg" />
+              <p data-testid="movie-rating"> {movie.vote_average}/10</p>
+            </div>
+            <button
+              className={
+                favoriteStatus[index]
+                  ? "favorite-button active"
+                  : "favorite-button"
+              }
+              onClick={() => toggleFavorite(index)}
+            >
+              {favoriteStatus[index] ? "❤️" : "🤍"}
+            </button>
           </div>
         </div>
       ))}
