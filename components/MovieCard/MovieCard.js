a593a8cce6ff5a0caf7141ab3dfbe078c8602430
Dorcas Bamisile <bamisiledorcas@gmail.com>
2023-09-15 20:29:55 +0100

    MovieCard

--- components/MovieCard/MovieCard.js
+++ components/MovieCard/MovieCard.js
@@ -1,7 +1,8 @@
-"use client";
+// MovieCard.js
 
 import React, { useState } from "react";
 import "./MovieCard.css";
+import Link from "next/link";
 
 const MovieCard = ({ movies }) => {
   const [favoriteStatus, setFavoriteStatus] = useState(
@@ -22,42 +23,43 @@ const MovieCard = ({ movies }) => {
     );
   }
 
-  console.log(movies);
+  const displayedMovies = movies.slice(0, 10);
+
   return (
     <div className="movie-card" data-testid="movie-card">
-      {movies.map((movie, index) => (
-        <div key={index} className="movie-list">
-          <div className="moviee">
-            <img
-              src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-              alt="movie"
-              data-testid="movie-poster"
-            />
-            <p data-testid="movie-release-year"> {movie.release_date}</p>
-            <h2 className="movie-title" data-testid="movie-title">
-              {movie.title}
-            </h2>
-            {/* <p data-testid="movie-overview" className="movie-overview">
-              {movie.overview}
-            </p> */}
-            <p data-testid="movie-genre">{movie.genre}</p>
-            <div className="rating">
-              <img src="imdb.svg" />
-              <p data-testid="movie-rating"> {movie.vote_average}/10</p>
+      <div className="movie-list-grid">
+        {displayedMovies.map((movie, index) => (
+          <div key={index} className="movie-item">
+            <div className="moviee">
+              <Link href={`/movies/${movie.id}`}>
+                <img
+                  src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+                  alt="movie"
+                  data-testid="movie-poster"
+                />
+              </Link>
+              <p data-testid="movie-release-date"> {movie.release_date}</p>
+              <h2 className="movie-title" data-testid="movie-title">
+                {movie.title}
+              </h2>
+              <div className="rating">
+                <img src="imdb.svg" alt="IMDB Logo" />
+                <p> {movie.vote_average}/10</p>
+              </div>
+              <button
+                className={
+                  favoriteStatus[index]
+                    ? "favorite-button active"
+                    : "favorite-button"
+                }
+                onClick={() => toggleFavorite(index)}
+              >
+                {favoriteStatus[index] ? "❤️" : "🤍"}
+              </button>
             </div>
-            <button
-              className={
-                favoriteStatus[index]
-                  ? "favorite-button active"
-                  : "favorite-button"
-              }
-              onClick={() => toggleFavorite(index)}
-            >
-              {favoriteStatus[index] ? "❤️" : "🤍"}
-            </button>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
--- pages/HomePage.js
+++ pages/HomePage.js
@@ -16,7 +16,7 @@ const HomePage = () => {
   useEffect(() => {
     const fetchPopularMovies = async () => {
       const response = await fetch(
-        `https://api.themoviedb.org/3/movie/popular?api_key=85b27a45d9bec629452daa02247b315c&language=en-US&page=1`
+        `https://api.themoviedb.org/3/movie/top_rated?api_key=85b27a45d9bec629452daa02247b315c&language=en-US&page=1`
       );
       const data = await response.json();
       setMovies(data.results);
@@ -33,7 +33,7 @@ const HomePage = () => {
     }
 
     const response = await fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=85b27a45d9bec629452daa02247b315c&language=en-US&query=${encodeURIComponent(
+      `https://api.themoviedb.org/3/movie/top_rated?api_key=85b27a45d9bec629452daa02247b315c&language=en-US&query=${encodeURIComponent(
         searchValue
       )}&page=1`
     );
