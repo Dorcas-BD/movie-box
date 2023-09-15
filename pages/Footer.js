3e145ef6e943903161ac830ef7e2661413fcad27
Dorcas Bamisile <bamisiledorcas@gmail.com>
2023-09-15 21:58:24 +0100

    Added  Footer

---
+++ pages/Footer.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "../styles/Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="f-container">
+        <div className="f-one">
+          <a href="https://www.facebook.com">
+            <i className="fab fa-facebook"></i>
+          </a>
+          <a href="https://www.instagram.com">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="https://www.twitter.com">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="https://www.youtube.com">
+            <i className="fab fa-youtube"></i>
+          </a>
+        </div>
+        <div className="f-two">
+          <p>Conditions of Use</p>
+          <p>Privacy & Policy</p>
+          <p>Press Room</p>
+        </div>
+        <div className="f-three">
+          <p>&copy; 2021 MovieBox by Dorcas Bamisile </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
